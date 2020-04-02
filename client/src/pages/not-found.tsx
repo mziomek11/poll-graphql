dda881271186b73aca6bdc1f3dcff07c5fcde15a
hommat <zmateusz1111@gmail.com>
2020-04-02 11:13:22 +0200

    Update 404 page

--- client/src/pages/not-found.tsx
+++ client/src/pages/not-found.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+
+import PageHeading from '../components/heading/Page';
+
 const NotFoundPage = () => {
-  return <main>Page not found</main>;
+  return (
+    <Box component="main" textAlign="center">
+      <PageHeading>404</PageHeading>
+      <Typography>Page does not exists</Typography>
+    </Box>
+  );
 };
 
 export default NotFoundPage;
